7fd17b713390144d2779e372dcf097d2d174494b
Wellisson Ribeiro <welleh10@gmail.com>
2023-01-23 17:54:23 -0300

    update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function runReaderGenerator(): void
 		}
 	}
 
-	echo "Leitura usando Generator";
+	echo "Leitura usando Generator" . PHP_EOL;
 	echo "Memória utilizada: " . round(memory_get_peak_usage() / 1024, 0) . "KB" . PHP_EOL;
 	echo "Tamanho do arquivo: " . round(filesize('./bigger-file.csv') / 1024 / 1024, 0) . "MB" . PHP_EOL;
 }
@@ -33,7 +33,7 @@ function runReaderFiber(): void
 	    printf('%s fez %d let it rip %s', $fiberLine[0], $fiberLine[1], PHP_EOL);
 	}
 
-	echo "Leitura usando Fiber";
+	echo "Leitura usando Fiber" . PHP_EOL;
 	echo "Memória utilizada: " . round(memory_get_peak_usage() / 1024, 0) . "KB" . PHP_EOL;
 	echo "Tamanho do arquivo: " . round(filesize('./bigger-file.csv') / 1024 / 1024, 0) . "MB" . PHP_EOL;
 }
