fdfc358cc5adecaecfd4d98b28478541ace98014
Wellisson Ribeiro <welleh10@gmail.com>
2023-01-24 07:45:08 -0300

    update part II

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use App\FileReaderFiber;
 
 require_once './vendor/autoload.php';
 
-function runReaderGenerator(): void
+function runReaderGenerator(): array
 {
 	$generatorReader = new FileReaderGenerator('./bigger-file.csv');
 	$generatorContents = $generatorReader->readfile();
@@ -16,12 +16,13 @@ function runReaderGenerator(): void
 		}
 	}
 
-	echo "Leitura usando Generator" . PHP_EOL;
-	echo "Memória utilizada: " . round(memory_get_peak_usage() / 1024, 0) . "KB" . PHP_EOL;
-	echo "Tamanho do arquivo: " . round(filesize('./bigger-file.csv') / 1024 / 1024, 0) . "MB" . PHP_EOL;
+	return [
+		'memory' => round(memory_get_peak_usage() / 1024, 0) . "KB",
+		'fileLength' => round(filesize('./bigger-file.csv') / 1024 / 1024, 0) . "MB"
+	];
 }
 
-function runReaderFiber(): void
+function runReaderFiber(): array
 {	
 	$fiberReader = new FileReaderFiber('./bigger-file.csv');
 	$fiberContents = new Fiber(function () use ($fiberReader) {
@@ -33,13 +34,25 @@ function runReaderFiber(): void
 	    printf('%s fez %d let it rip %s', $fiberLine[0], $fiberLine[1], PHP_EOL);
 	}
 
+	return [
+		'memory' => round(memory_get_peak_usage() / 1024, 0) . "KB",
+		'fileLength' => round(filesize('./bigger-file.csv') / 1024 / 1024, 0) . "MB"
+	];
+
 	echo "Leitura usando Fiber" . PHP_EOL;
 	echo "Memória utilizada: " . round(memory_get_peak_usage() / 1024, 0) . "KB" . PHP_EOL;
 	echo "Tamanho do arquivo: " . round(filesize('./bigger-file.csv') / 1024 / 1024, 0) . "MB" . PHP_EOL;
 }
 
+$generatorResult = runReaderGenerator();
+$fiberResult = runReaderFiber();
+
 echo "----------------------------" . PHP_EOL;
-runReaderGenerator();
+echo "Leitura usando Generator" . PHP_EOL;
+echo "Memória utilizada: " . $generatorResult['memory'] . PHP_EOL;
+echo "Tamanho do arquivo: " . $fiberResult['fileLength'] . PHP_EOL;
 echo "----------------------------" . PHP_EOL;
-runReaderFiber();
+echo "Leitura usando Fiber" . PHP_EOL;
+echo "Memória utilizada: " . $fiberResult['memory'] . PHP_EOL;
+echo "Tamanho do arquivo: " . $fiberResult['fileLength'] . PHP_EOL;
 echo "----------------------------" . PHP_EOL;
